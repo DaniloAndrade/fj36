01460da4f73ce0a55aed6378ca2cb8713dd6d7b3
Danilo dos Santos Andrade <danilo.s.andrade@gmail.com>
2016-03-06 14:53:34 -0300

    implementado forma de pagamento

--- fj36-livraria/src/main/java/br/com/caelum/livraria/rest/ClienteRest.java
+++ fj36-livraria/src/main/java/br/com/caelum/livraria/rest/ClienteRest.java
@@ -2,11 +2,16 @@ package br.com.caelum.livraria.rest;
 
 import java.io.Serializable;
 
+import br.com.caelum.livraria.modelo.Link;
 import org.springframework.stereotype.Component;
 import org.springframework.context.annotation.Scope;
 
 import br.com.caelum.livraria.modelo.Pagamento;
 import br.com.caelum.livraria.modelo.Transacao;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 
 @Component
 @Scope("request")
@@ -19,11 +24,24 @@ public class ClienteRest implements Serializable {
 	private static final String ENTRY_POINT = "/pagamentos/";
 
 	public Pagamento criarPagamento(Transacao transacao) {
-		return null;
+		Client client = ClientBuilder.newClient();
+		Pagamento pagamento = client.target(SERVER_URI + ENTRY_POINT)
+				.request()
+				.buildPost(Entity.json(transacao))
+				.invoke(Pagamento.class);
+		System.out.println("Pagamento criado, id: " +pagamento.getId());
+		return pagamento;
 	}
 
 	public Pagamento confirmarPagamento(Pagamento pagamento) {
-		return null;
+		Link linkConfirmar = pagamento.getLinkPeloRel("confirmar");
+		Client client = ClientBuilder.newClient();
+		Pagamento resposta = client.target(SERVER_URI + linkConfirmar.getUri())
+				.request()
+				.build(linkConfirmar.getMethod())
+				.invoke(Pagamento.class);
+		System.out.println("Pagamento confirmado, id: "+ resposta.getId());
+		return resposta;
 	}
 
 }
