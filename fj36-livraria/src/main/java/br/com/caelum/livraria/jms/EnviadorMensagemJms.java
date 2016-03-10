42fd4819a2291668e49a61f799d82c43f64dfe30
Danilo dos Santos Andrade <danilo.s.andrade@gmail.com>
2016-03-09 22:40:41 -0300

    finalização de implementação do exercicio 5.22

--- fj36-livraria/src/main/java/br/com/caelum/livraria/jms/EnviadorMensagemJms.java
+++ fj36-livraria/src/main/java/br/com/caelum/livraria/jms/EnviadorMensagemJms.java
@@ -2,19 +2,36 @@ package br.com.caelum.livraria.jms;
 
 import java.io.Serializable;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import br.com.caelum.livraria.jaxb.SerializadorXml;
 import br.com.caelum.livraria.modelo.Pedido;
 
+import javax.jms.*;
+
 @Component
 @Lazy(true)
 public class EnviadorMensagemJms implements Serializable {
+
+//	@Qualifier("hornetQConnectionFactory")
+	@Autowired
+	private ConnectionFactory factory;
+//	@Qualifier("topicoLivraria")
+	@Autowired
+	private Topic topic;
 
 	private static final long serialVersionUID = 1L;
 
 	public void enviar(Pedido pedido) {
 		// código para enviar a mensagem
+		System.out.println("JMS: Enviando pedido: "+ pedido);
+		try (JMSContext context = factory.createContext("jms","jms2")){
+			JMSProducer producer = context.createProducer();
+			producer.setProperty("formato", pedido.getFormato());
+			producer.send(topic,pedido.toString());
+		}
 	}
 }
